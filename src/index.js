7da3e56a35d0fbe9bc707ec3cc80f640fffcbbe4
sharma-1456 <vishalsharma74880@gmail.com>
2024-06-05 14:43:28 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // second option two connect database
 import dotenv from 'dotenv'
 import connectDB from './db/index.js'
+import app from './app.js'
 
 // fist option to connect database
 // import mongoose from 'mongoose'
@@ -12,6 +13,18 @@ dotenv.config({
     path:'./env'
 })
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(` server is running at port: ${porcess.env.PORT}`)
+    })
+    app.on("error",(error)=>{
+            console.log("ERROR::",error);
+            throw error;
+    })
+})
+.catch((err)=>{
+    console.log('MONGODB connection failed!!!',err)
+})
 
 
 
